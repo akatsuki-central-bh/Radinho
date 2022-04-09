344dff8aacb534e609cccfb0afe2d54df3503b14
leandrohorizon <leandro.mcontato@gmail.com>
2022-04-09 14:27:48 -0300

    Cria interface e método para envio de mensagem

--- client_linux.py
+++ client_linux.py
@@ -1,16 +1,34 @@
+from tkinter import *
+from tkinter import ttk
+
 import socket
+
 HOST = '192.168.1.255'  # Endereco IP do Servidor
 PORT = 5000            # Porta que o Servidor esta
 
 udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 udp.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-breakpoint()
 
 dest = (HOST, PORT)
-print('Conectado\n')
 
-msg = 'leanddro conectado..'
+def send_message():
+  INPUT = inputtxt.get("1.0", "end-1c")
+  udp.sendto (INPUT.encode(), dest)
+  outputtxt.insert(END, f"Você: {INPUT}\n")
 
-while True:
-  udp.sendto (msg.encode(), dest)
-  msg = input()
+root = Tk()
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+ttk.Label(frm, text="Chat dos menores").grid(column=0, row=0)
+
+outputtxt = Text(frm, height = 20, width = 30)
+outputtxt.grid(column=0, row=1)
+
+inputtxt = Text(frm, height = 1, width = 25)
+inputtxt.grid(column=0, row=2)
+
+ttk.Button(frm, text="enviar", command = lambda:send_message()).grid(column=1, row=2)
+
+root.mainloop()
+
+
